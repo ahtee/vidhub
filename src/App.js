87c8ec9911fba557ee96c1be0bf3efbf588fb76f
ahtee <benottdev@gmail.com>
2018-08-10 15:48:29 -0400

    Update styles

--- src/App.js
+++ src/App.js
@@ -7,27 +7,27 @@ export default class App extends Component {
     return (
       <div className="container mt-4">
         <Form row>
-          <FormGroup className="mb-3 mr-sm-2 mb-sm-0">
+          <FormGroup className="mt-3 mr-sm-2 mb-sm-0">
             <Label for="middleware-id">Owner</Label>
-            <Col sm={12} md={6}>
+            <Col sm={12} md={12}>
               <Input type="text" name="password" id="middleware-id" />
             </Col>
           </FormGroup>
-          <FormGroup className="mb-3 mr-sm-2 mb-sm-0">
+          <FormGroup className="mt-3 mr-sm-2 mb-sm-0">
             <Label for="application-name">Application Identifier (UAI, App ID) </Label>
-            <Col sm={12} md={6}>
+            <Col sm={12} md={12}>
               <Input type="text" name="application-name" id="application-name" />
             </Col>
           </FormGroup>
-          <FormGroup className="mb-3 mr-sm-2 mb-sm-0">
+          <FormGroup className="mt-3 mr-sm-2 mb-sm-0">
             <Label for="middleware-id">Owner</Label>
-            <Col sm={12} md={6}>
+            <Col sm={12} md={12}>
               <Input type="text" name="password" id="middleware-id" />
             </Col>
           </FormGroup>
-          <FormGroup className="mb-3 mr-sm-2 mb-sm-0">
+          <FormGroup className="mt-3 mr-sm-2 mb-sm-0">
             <Label for="middlewareSelect">Middleware</Label>
-            <Col sm={12} md={6}>
+            <Col sm={12} md={12}>
               <Input type="select" name="selectMiddleware" id="middlewareSelect" required>
                 <option disabled selected value> -- select an option -- </option>
                 <option>Apache</option>
@@ -38,7 +38,7 @@ export default class App extends Component {
               </Input>
             </Col>
           </FormGroup>
-          <Col sm={12} md={6}>
+          <Col sm={12} md={12} mt-3>
             <Button className="btn btn-success btn-lg btn-block mt-3">Generate</Button>
           </Col>
         </Form>
