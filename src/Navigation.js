3e21965d7008ebe9d220b67deb6f974ccefe6732
ahtee <ahtee@ahtees-mbp.home>
2018-10-29 19:02:19 -0400

    Changing navbar

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,20 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './VidHub.JPG';
-import {
-  Button,
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Form,
-  FormGroup,
-  Label,
-  Input
- } from 'reactstrap';
+import user from './default-user.png';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 export default class App extends Component {
   constructor(props) {
@@ -34,24 +22,82 @@ export default class App extends Component {
 
   render() {
     return (
-        <Navbar color="dark" dark expand="md">
-          <NavbarBrand><img src={logo} alt="logo" class="logo" height="40"/></NavbarBrand>
-          
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink className="mt-1" activeClassName="active" href="/gen" exact>Generate</NavLink>
-            </NavItem>
-              <NavItem>
-                <NavLink activeClassName="active" href="/" exact><i class="fas fa-user-circle fa-2x"></i></NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/ahtee/vidhub" target="_blank"><i className="fab fa-github fa-2x"></i></NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="d-flex flex-justify-between flex-auto">
+          <div className="d-flex">
+            <a className="navbar-brand" href="/"><img src={logo} alt="VidHub" height="40"/></a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav d-flex pl-2 pr-2 flex-items-center text-bold list-style-none">
+                <li className="nav-item active">
+                  <a className="nav-link" href="/">Trending <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/">Discover</a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Live
+                  </a>
+                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a className="dropdown-item" href="/">Games</a>
+                    <a className="dropdown-item" href="/">Vlogs</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="/">Code</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search videos/streams..." aria-label="Search" />
+            <input className="btn btn-outline-success my-2 my-sm-0" type="hidden" />
+          </form>
+        </div>
+        <div className="d-flex justify-content-end ml-auto">
+          <ul className="navbar-nav d-flex pl-2 pr-2 flex-items-center text-bold list-style-none">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img src={user} className="nav-user-login" alt="Log In" width="30" height="30" />
+              </a>
+              <div className="dropdown-menu p-3" aria-labelledby="navbarDropdown">
+                <Form>
+                  <FormGroup>
+                    <Label htmlFor="user-email">Username or email:</Label>
+                    <Input type="email" name="user-email" id="user-email" required/>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label htmlFor="user-password">Password</Label>
+                    <Input type="password" name="user-password" id="user-password" required/>
+                  </FormGroup>
+                  <div class="remember-me checkbox">
+                    <FormGroup className="ml-4">
+                      <Label htmlFor="remember">
+                        <Input type="checkbox" name="remember" id="remember" value="0" />
+                        <span className="pull-left">Remember me</span>
+                        <div className="pull-right forgot-password">
+                          <a href="/">Forgot your password?</a>
+                        </div>
+                      </Label>
+                    </FormGroup>
+                  </div>
+                  <Button type="submit" className="btn btn-success" block="true">Sign In</Button>
+                </Form>
+              </div>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="/">Trending <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Discover</a>
+            </li>
+          </ul>
+        </div>
+      </nav> 
+        
     );
   }
 }
